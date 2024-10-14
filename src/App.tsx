c4c0c8f74a8300112799c2ba7838362dc67396e3
Nguyễn Khánh Quy <120696079+nguyenkhanhquy@users.noreply.github.com>
2024-10-14 13:37:07 +0700

    add: Footer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { BrowserRouter } from "react-router-dom";
+
 import Navbar from "./components/layouts/Page/Navbar";
+import Footer from "./components/layouts/Page/Footer";
 
 function App() {
     return (
         <>
             <BrowserRouter>
                 <Navbar />
+                <Footer />
             </BrowserRouter>
         </>
     );
